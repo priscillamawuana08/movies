7c84fbb7c519fbe87a66f8abe0154638e5520422
Samuel Afotey Laryea <afoteylaryeasamuel@gmail.com>
2023-06-05 23:30:51 +0100

    Getting chapters of anime with open souce anime api

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, { Component,useState } from 'react';
+import React, { Component, useState } from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer/Footer';
 // import NavbarElement from './Components/Navbar/NavbarElement';
-import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './pages/Login';
 import Registration from './pages/Registration';
 import Home from './pages/Home';
 import About from './pages/About';
 import Service from './pages/Services';
-import './App.css'; 
+import Movies from './pages/movies';
+import './App.css';
 
 class App extends Component {
-  
+
   render() {
     return (
-      
-     <Router>
-      <div className='App'>
-        <Navbar/>
-        <Footer/>
+
+      <Router>
+        <div className='App'>
+          {/* <Navbar /> */}
+          {/* <Footer /> */}
+          <Routes>
+            <Route exact path='/' element={< Home />}></Route>
+            <Route exact path='/login' element={< Login />}></Route>
+            <Route exact path='/register' element={< Registration />}></Route>
+            <Route exact path='/about' element={< About />}></Route>
+            <Route exact path='/service' element={< Service />}></Route>
+          </Routes>
+        </div>
         <Routes>
-          <Route exact path='/' element={< Home />}></Route>
-          <Route exact path='/login' element={< Login />}></Route>
-          <Route exact path='/register' element={< Registration />}></Route>
-          <Route exact path='/about' element={< About/>}></Route>
-          <Route exact path='/service' element={< Service/>}></Route>
+
+          <Route exact path='/movies' element={< Movies />}></Route>
         </Routes>
-      </div>
-     </Router>
-      
+      </Router>
+
     );
   }
 }
---
+++ src/pages/movies.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from 'react';
+
+// import './About.css'; 
+  
+const Movies = () => {
+
+const [data, setData] = useState(null);
+
+useEffect(() => {
+    getChapter();
+  }, []);
+
+const getChapter = async () => {
+    try {
+      const response = await fetch('https://finalspaceapi.com/api/v0/episode');
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.log('Error fetching data:', error);
+    }
+  };
+
+    return (
+
+
+        <div className='coontainer'>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Features</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div>
+               {
+                data && data.map((item)=>(
+                    <div className="card w-50">
+                    <img src={item.img_url} className="card-img-top" alt="chapter image" width={80} />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                    <ul className="list-group list-group-flush">
+                        <li className="list-group-item">An item</li>
+                        <li className="list-group-item">A second item</li>
+                        <li className="list-group-item">A third item</li>
+                    </ul>
+                    <div className="card-body">
+                        <a href="#" className="card-link">Card link</a>
+                        <a href="#" className="card-link">Another link</a>
+                    </div>
+                </div>
+                ))
+               }
+            </div>
+        </div>
+
+    );
+}
+
+
+export default Movies;
